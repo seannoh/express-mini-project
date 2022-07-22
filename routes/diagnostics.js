ce87c417b4c27e747111bb4e01410d273c3f2b84
Matt F <fiaschettima@gmail.com>
2022-07-22 15:06:08 -0400

    diag post append

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -1,7 +1,8 @@
 const diagnostics = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
-
+const fs = require('fs')
+const errors = require('../db/diagnostics.json')
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
@@ -9,7 +10,33 @@ diagnostics.get('/', (req, res) => {
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
-  // TODO: Logic for appending data to the db/diagnostics.json file
+  const { time, error_id, errors} = req.body
+  const newEror = {
+    time,
+    error_id,
+    errors,
+  }
+  const currentErrs = JSON.stringify(newEror, null, '\t')
+  fs.readFile('../db/diagnostics.json', (err, res) => {
+    if(err){
+      console.log(err)
+    }else{    
+      var prevErrs = JSON.parse(res)
+      prevErrs.push(newEror)
+      errors = prevErrs;
+      fs.writeFile('../db/diagnostics.json', JSON.stringify(errors, null, 4), (printeror) =>
+        {
+          printeror ? console.error(printeror) : console.info('Write Passed') 
+        })
+  }})
+ 
 });
-
+  // {
+  //   "time": 1616087173408,
+  //   "error_id": "249911fc-ce9d-4905-a934-745845b41c7a",
+  //   "errors": {
+  //     "tip:": "",
+  //     "topic": "Gaming is not a valid topic",
+  //     "username": ""
+  //   }
 module.exports = diagnostics;
